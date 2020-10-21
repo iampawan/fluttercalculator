5bd1fc9ec5faa1bd639da739629e6a6e80e1db0c
nishant chavan <nishantscg@gmail.com>
2020-10-21 20:51:50 +0530

    app content overflow by 10px

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -56,78 +56,80 @@ class HomePageState extends State<HomePage> {
       appBar: new AppBar(
         title: new Text("Calculator"),
       ),
-      body: new Container(
-        padding: const EdgeInsets.all(40.0),
-        child: new Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text(
-              "Output : $sum",
-              style: new TextStyle(
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.purple),
-            ),
-            new TextField(
-              keyboardType: TextInputType.number,
-              decoration: new InputDecoration(hintText: "Enter Number 1"),
-              controller: t1,
-            ),
-            new TextField(
-              keyboardType: TextInputType.number,
-              decoration: new InputDecoration(hintText: "Enter Number 2"),
-              controller: t2,
-            ),
-            new Padding(
-              padding: const EdgeInsets.only(top: 20.0),
-            ),
-            new Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                new MaterialButton(
-                  child: new Text("+"),
-                  color: Colors.greenAccent,
-                  onPressed: doAddition,
-                ),
-                new MaterialButton(
-                  child: new Text("-"),
-                  color: Colors.greenAccent,
-                  onPressed: doSub,
-                ),
-              ],
-            ),
-            new Padding(
-              padding: const EdgeInsets.only(top: 20.0),
-            ),
-            new Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                new MaterialButton(
-                  child: new Text("*"),
-                  color: Colors.greenAccent,
-                  onPressed: doMul,
-                ),
-                new MaterialButton(
-                  child: new Text("/"),
-                  color: Colors.greenAccent,
-                  onPressed: doDiv,
-                ),
-              ],
-            ),
-            new Padding(
-              padding: const EdgeInsets.only(top: 20.0),
-            ),
-            new Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                new MaterialButton(
-                  child: new Text("Clear"),
-                  color: Colors.greenAccent,
-                  onPressed: doClear,
-                ),
-              ],
-            )
-          ],
+      body: SingleChildScrollView(
+              child: new Container(
+          padding: const EdgeInsets.all(40.0),
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new Text(
+                "Output : $sum",
+                style: new TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.purple),
+              ),
+              new TextField(
+                keyboardType: TextInputType.number,
+                decoration: new InputDecoration(hintText: "Enter Number 1"),
+                controller: t1,
+              ),
+              new TextField(
+                keyboardType: TextInputType.number,
+                decoration: new InputDecoration(hintText: "Enter Number 2"),
+                controller: t2,
+              ),
+              new Padding(
+                padding: const EdgeInsets.only(top: 20.0),
+              ),
+              new Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  new MaterialButton(
+                    child: new Text("+"),
+                    color: Colors.greenAccent,
+                    onPressed: doAddition,
+                  ),
+                  new MaterialButton(
+                    child: new Text("-"),
+                    color: Colors.greenAccent,
+                    onPressed: doSub,
+                  ),
+                ],
+              ),
+              new Padding(
+                padding: const EdgeInsets.only(top: 20.0),
+              ),
+              new Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  new MaterialButton(
+                    child: new Text("*"),
+                    color: Colors.greenAccent,
+                    onPressed: doMul,
+                  ),
+                  new MaterialButton(
+                    child: new Text("/"),
+                    color: Colors.greenAccent,
+                    onPressed: doDiv,
+                  ),
+                ],
+              ),
+              new Padding(
+                padding: const EdgeInsets.only(top: 20.0),
+              ),
+              new Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  new MaterialButton(
+                    child: new Text("Clear"),
+                    color: Colors.greenAccent,
+                    onPressed: doClear,
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
